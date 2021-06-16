f2171f7f4e91734ec385f3bff2e4f1c54aa42132
AdelaMester <adelamester@yahoo.co.uk>
2021-06-16 10:56:01 +0000

    Added POST, PUT and DELETE for assay_results

--- application.py
+++ application.py
@@ -1,11 +1,31 @@
 from flask import Flask, request, jsonify
 import os
 import sqlite3
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, reqparse
 
 # Configure application and Api
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Exscentia API',description="The v1.0 Compound API")
+
+# Initialize RequestParser to create or update a new compound
+parser = reqparse.RequestParser()
+parser.add_argument('smiles', location='json', required=True, help='Rate cannot be converted')
+parser.add_argument('molecular_weight', location='json', required=True, type= float)
+parser.add_argument('ALogP', location='json', required=True,  type= float)
+parser.add_argument('molecular_formula', location='json', required=True)
+parser.add_argument('num_rings', location='json', required=True, type= int)
+parser.add_argument('image', location='json', required=True)
+
+
+# Initialize RequestParser to create or update a new assay_result
+parserUpdate = reqparse.RequestParser()
+parserUpdate.add_argument('result_id', location='json', required=True, help='Rate cannot be converted', type= int)
+parserUpdate.add_argument('target', location='json', required=True)
+parserUpdate.add_argument('result', location='json', required=True)
+parserUpdate.add_argument('operator', location='json', required=True)
+parserUpdate.add_argument('value', location='json', required=True, type= int)
+parserUpdate.add_argument('unit', location='json', required=True)
+
 
 @api.route('/compounds')
 class Compounds(Resource):
@@ -27,28 +47,6 @@ class Compounds(Resource):
         conn.close()
         print(result)
         return jsonify(result)
-
-    def post(self):
-
-        """Creates a compound"""
-
-        compound_id = request.json['compound_id']
-        smiles = request.json['smiles']
-        molecular_weight = request.json['molecular_weight']
-        ALogP = request.json['ALogP']
-        molecular_formula = request.json['molecular_formula']
-        num_rings = request.json['num_rings']
-        image = request.json['image']
-
-        conn = sqlite3.connect('json.db')
-        print ("Opened database successfully")
-
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO compounds (compound_id, smiles, molecular_weight, ALogP, molecular_formula, num_rings, image) VALUES (?,?,?,?,?,?,?)", (compound_id, smiles, molecular_weight, ALogP, molecular_formula, num_rings, image))
-        allCompounds = cursor.fetchall()
-        conn.commit()
-        print("New compound created")
-        return jsonify("Insert done")
 
 
 @api.route("/compound/<int:num>")
@@ -74,17 +72,44 @@ class Compounds_id(Resource):
         return jsonify(result)
 
 
+    @api.expect(parser)
+    def post(self, num):
+
+        """Creates a compound"""
+
+        args = parser.parse_args()
+        print(args)
+        compound_id = num
+        smiles = args['smiles']
+        molecular_weight = args['molecular_weight']
+        ALogP = args['ALogP']
+        molecular_formula = args['molecular_formula']
+        num_rings = args['num_rings']
+        image = args['image']
+
+        conn = sqlite3.connect('json.db')
+        print ("Opened database successfully")
+
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO compounds (compound_id, smiles, molecular_weight, ALogP, molecular_formula, num_rings, image) VALUES (?,?,?,?,?,?,?)", (compound_id, smiles, molecular_weight, ALogP, molecular_formula, num_rings, image))
+        conn.commit()
+        print("New compound created")
+        return jsonify("Insert done")
+
+
+    @api.expect(parser)
     def put(self, num):
 
         """Updates the details for the specified compound_id"""
 
+        args = parser.parse_args()
         compound_id = num
-        smiles = request.json['smiles']
-        molecular_weight = request.json['molecular_weight']
-        ALogP = request.json['ALogP']
-        molecular_formula = request.json['molecular_formula']
-        num_rings = request.json['num_rings']
-        image = request.json['image']
+        smiles = args['smiles']
+        molecular_weight = args['molecular_weight']
+        ALogP = args['ALogP']
+        molecular_formula = args['molecular_formula']
+        num_rings = args['num_rings']
+        image = args['image']
 
         conn = sqlite3.connect('json.db')
         print ("Opened database successfully")
@@ -135,6 +160,67 @@ class Compounds_id(Resource):
         return jsonify(result)
 
 
+    @api.expect(parserUpdate)
+    def post(self,num):
+
+        """Creates an assay_result for the specified compound_id"""
+
+        args = parserUpdate.parse_args()
+        print(args)
+        compound_id = num
+        result_id = args['result_id']
+        target = args['target']
+        result = args['result']
+        operator = args['operator']
+        value = args['value']
+        unit = args['unit']
+
+        conn = sqlite3.connect('json.db')
+        print ("Opened database successfully")
+
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO assay_results (compound_id, result_id, target, result, operator, value, unit) VALUES (?,?,?,?,?,?,?)", (compound_id, result_id, target, result, operator, value, unit))
+        conn.commit()
+        print("New compound created")
+        return jsonify("Insert done")
 
 
+    @api.expect(parserUpdate)
+    def put(self, num):
 
+        """Updates the details for assay_results of the specified compound_id"""
+
+
+        args = parserUpdate.parse_args()
+        print(args)
+        compound_id = num
+        result_id = args['result_id']
+        target = args['target']
+        result = args['result']
+        operator = args['operator']
+        value = args['value']
+        unit = args['unit']
+
+        conn = sqlite3.connect('json.db')
+        print ("Opened database successfully")
+
+        cursor = conn.cursor()
+        cursor.execute("UPDATE assay_results SET target=?, result=?, operator=?, value=?, unit=? WHERE compound_id=? AND result_id=?", (target, result, operator, value, unit, num, result_id))
+        conn.commit()
+        conn.close()
+        return jsonify("Update done")
+
+    def delete(self, num):
+
+        """DELETE assay_result data for the specified compound_id"""
+
+        compound_id = num
+        conn = sqlite3.connect('json.db')
+        print ("Opened database successfully")
+
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM comp WHERE compound_id=?", (compound_id,))
+        cursor.execute("DELETE FROM assay_result WHERE compound_id=?", (compound_id,))
+        conn.commit()
+        conn.close()
+        return jsonify("Deleted")
